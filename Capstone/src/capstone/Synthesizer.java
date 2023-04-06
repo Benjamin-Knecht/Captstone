a852fa826578614b2f9bb9c87e0360c0a0656211
Benjamin Knecht <122293873+Benjamin-Knecht@users.noreply.github.com>
2023-04-06 13:44:25 -0400

    fixed ui

--- Capstone/src/capstone/Synthesizer.java
+++ Capstone/src/capstone/Synthesizer.java
@@ -317,19 +317,19 @@ public class Synthesizer {
         legend.setBorder(Utils.WindowDesign.LINE_BORDER);
         JLabel legendText = new JLabel("Tuning & Pitch Legend");
         String[][] array = {
-            {"(+)Tuning","(x)Freq Mult","(#)Notes"},
-            {"0.083","1.060","1 half"},
-            {"0.167","1.122","2 whole"},
-            {"0.250","1.189","3 min.3rd"},
-            {"0.333","1.260","4 maj.3rd"},
-            {"0.417","1.335","5 fourth"},
-            {"0.500","1.414","6 tritone"},
-            {"0.583","1.500","7 fifth"},
-            {"0.667","1.587","8 min.6th"},
-            {"0.750","1.682","9 maj.6th"},
-            {"0.833","1.782","10 min.7th"},
-            {"0.917","1.888","11 maj.7th"},
-            {"1.000","2.000","12 octave"},
+            {"Tuning","Frequency Multiplier","Notes"},
+            {"+0.083","x1.060","1 half"},
+            {"+0.167","x1.122","2 whole"},
+            {"+0.250","x1.189","3 min.3rd"},
+            {"+0.333","x1.260","4 maj.3rd"},
+            {"+0.417","x1.335","5 fourth"},
+            {"+0.500","x1.414","6 tritone"},
+            {"+0.583","x1.500","7 fifth"},
+            {"+0.667","x1.587","8 min.6th"},
+            {"+0.750","x1.682","9 maj.6th"},
+            {"+0.833","x1.782","10 min.7th"},
+            {"+0.917","x1.888","11 maj.7th"},
+            {"+1.000","x2.000","12 octave"},
         };
         /*String[][] array = {
             {"(+)Tuning","(x)Freq Mult","(#)Half Steps"},
